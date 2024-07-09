731bfcbe9512706d99e4f490115f2a140f07e900
Romeo Köppel <romeo98@bluewin.ch>
2024-07-09 21:13:05 +0200

    add flowbite svelte and navbar

--- webshop/src/main/webui/src/lib/navbar/navbar.svelte
+++ webshop/src/main/webui/src/lib/navbar/navbar.svelte
@@ -1,13 +1,17 @@
+<script>
+    import { Navbar, NavBrand, NavHamburger, NavUl, NavLi } from 'flowbite-svelte';
+</script>
 
-<style type="text/css">
-    .navbar
-</style>
-
-<nav class="navbar">
-    <div class="container">
-        <a class="navbar-brand" href="/">
-        </a>
-    </div>
-</nav>
-
-There it is a navbar component.
+<Navbar  >
+    <NavBrand href="/">
+        <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    </NavBrand>
+    <NavHamburger  />
+    <NavUl >
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/about">About</NavLi>
+        <NavLi href="/docs/components/navbar">Navbar</NavLi>
+        <NavLi href="/pricing">Pricing</NavLi>
+        <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+</Navbar>
