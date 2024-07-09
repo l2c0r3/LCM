ad51eec830e175d33955011fc82e232f6c4d8028
Romeo Köppel <romeo98@bluewin.ch>
2024-07-09 20:55:12 +0200

    work on navbar

---
+++ webshop/src/main/webui/src/lib/navbar/navbar.svelte
@@ -0,0 +1,13 @@
+
+<style type="text/css">
+    .navbar
+</style>
+
+<nav class="navbar">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+        </a>
+    </div>
+</nav>
+
+There it is a navbar component.
--- webshop/src/main/webui/src/routes/+page.svelte
+++ webshop/src/main/webui/src/routes/+page.svelte
@@ -1,3 +1,19 @@
+<svelte:head>
+    <title>Homepage of </title>/
+</svelte:head>
+
+<script lang="ts">
+    import Navbar from './../lib/navbar/navbar.svelte'
+
+</script>
+
+<Navbar/>
+
 <h1>Welcome to SvelteKit</h1>
+
+<button >Add</button>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Visit <a href="./sub-route">a sub route</a></p>
+
+
