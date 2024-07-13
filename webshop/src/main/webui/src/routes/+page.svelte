88018bad77aa50b7cbf22c05a43dcaf53ffecc23
Romeo Köppel <romeo98@bluewin.ch>
2024-07-13 22:14:16 +0200

    first layout with card

--- webshop/src/main/webui/src/routes/+page.svelte
+++ webshop/src/main/webui/src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
     import Videoheader from '$lib/videoheader/videoheader.svelte'
+    import {Card, Button, Toggle} from 'flowbite-svelte';
+    import {ArrowRightOutline} from 'flowbite-svelte-icons';
+
+    let vCard = true;
 </script>
 
 <svelte:head>
@@ -8,7 +12,23 @@
 
 <Videoheader></Videoheader>
 
-<h1>Welcome to b</h1>
+<main>
+    <h2>Unsere Produkte</h2>
+
+    <div class="space-y-4">
+        <Card img="/images/image-1.webp">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise
+                technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <Button>
+                Read more
+                <ArrowRightOutline class="w-6 h-6 ms-2 text-white"/>
+            </Button>
+        </Card>
+    </div>
+</main>
+
 
 
 
