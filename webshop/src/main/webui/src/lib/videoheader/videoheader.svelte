9dbb245f1cf1cd0fd045408f9686b628884447c5
Romeo Köppel <romeo98@bluewin.ch>
2024-07-13 11:01:19 +0200

    add navbar to video

--- webshop/src/main/webui/src/lib/videoheader/videoheader.svelte
+++ webshop/src/main/webui/src/lib/videoheader/videoheader.svelte
@@ -1,10 +1,27 @@
 <script>
-    import {Video} from "flowbite-svelte";
+    import { Navbar, NavBrand, NavHamburger, NavLi, NavUl, Video } from "flowbite-svelte";
 </script>
 
 <!-- https://dev.to/dailydevtips1/tailwind-css-full-screen-video-header-5539 -->
 
 <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+    <div class="absolute top-0 left-0 w-full z-30">
+        <Navbar class="bg-opacity-50 bg-black">
+            <NavBrand href="/">
+                <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+                <span class="self-center whitespace-nowrap text-xl font-semibold text-white">LCM Bienenhof</span>
+            </NavBrand>
+            <NavHamburger class="text-white"/>
+            <NavUl class="">
+                <NavLi class="text-white" href="/shop">Shop</NavLi>
+                <NavLi class="text-white" href="/gallery">Gallery</NavLi>
+                <NavLi class="text-white" href="/contact">Contact</NavLi>
+                <NavLi class="text-white" href="/about">About</NavLi>
+            </NavUl>
+        </Navbar>
+    </div>
+
+
     <div class="relative text-white bg-opacity-50 z-30 p-5 text-center">
         <div class="mb-6">
             <div class="text-7xl font-bold">
@@ -17,6 +34,7 @@
             </div>
         </div>
     </div>
+
     <Video src="https://media.istockphoto.com/id/1478750832/de/video/bienen-fliegen.mp4?s=mp4-640x640-is&k=20&c=11g4WY5YLZDdSm6YkGfDCiLEq8gCFMOnkOncaS-LRW8="
            autoplay
            loop
@@ -25,5 +43,3 @@
            class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
     ></Video>
 </header>
-
-
