5161e0f7b05f2cbdb53e98834538919920a4eb0d
Romeo Köppel <romeo98@bluewin.ch>
2024-07-13 10:47:23 +0200

    dont show the navbar in main

--- webshop/src/main/webui/src/lib/navbar/navbar.svelte
+++ webshop/src/main/webui/src/lib/navbar/navbar.svelte
@@ -1,16 +1,22 @@
-<script>
-    import { Navbar, NavBrand, NavHamburger, NavUl, NavLi } from 'flowbite-svelte';
+<script lang="ts">
+    import {Navbar, NavBrand, NavHamburger, NavUl, NavLi} from 'flowbite-svelte';
+    import { page } from '$app/stores';
+
+    let isHomePage: boolean;
+    $: isHomePage = $page.url.pathname === "/";
 </script>
 
-<Navbar  >
-    <NavBrand href="/">
-        <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-    </NavBrand>
-    <NavHamburger  />
-    <NavUl >
-        <NavLi href="/shop">Shop</NavLi>
-        <NavLi href="/gallery">Gallery</NavLi>
-        <NavLi href="/contact">Contact</NavLi>
-        <NavLi href="/about">About</NavLi>
-    </NavUl>
-</Navbar>
+{#if !isHomePage}
+    <Navbar>
+        <NavBrand href="/">
+            <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+        </NavBrand>
+        <NavHamburger/>
+        <NavUl>
+            <NavLi href="/shop">Shop</NavLi>
+            <NavLi href="/gallery">Gallery</NavLi>
+            <NavLi href="/contact">Contact</NavLi>
+            <NavLi href="/about">About</NavLi>
+        </NavUl>
+    </Navbar>
+{/if}
